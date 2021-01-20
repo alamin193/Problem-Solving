0378179aa868f14bb1a23ef9ddfa2c5c0e0f00df
Md. Al Amin Miah <mdalaminmiah909@gmail.com>
2021-01-20 20:52:19 +0600

    done

--- URI/1001.c
+++ URI/1001.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int A,B,X;
-    scanf("%d",&A);
-    scanf("%d",&B);
-    X=A+B;
-    printf("X = %d\n",X);
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("X = %d\n",a+b);
     return 0;
 }
